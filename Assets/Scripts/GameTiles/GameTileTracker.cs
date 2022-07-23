1243e0b64eeaf50337505e68bd27c06b8f9621cf
Inferni Bitterdark <ralcoope@gmail.com>
2022-07-23 13:52:45 -0400

    Added a dictionary to tracker for watching highlighted tiles for the upcoming pathfinding functions

--- Assets/Scripts/GameTiles/GameTileTracker.cs
+++ Assets/Scripts/GameTiles/GameTileTracker.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 
 /// <summary>
 /// Script attached to the Tilemap by the Game Tile Manager for tracking all the Game Object GameTiles in a scene by their grid coordinates
+/// And all of the currently highlighted game tile renderers with their colors
 /// Particularly critical for the use of pathfinding algorithms
 /// </summary>
 public class GameTileTracker : MonoBehaviour
 {
     public Dictionary<Vector2Int, GameObject> GameTileDictionary = new Dictionary<Vector2Int, GameObject>();
+    public Dictionary<Renderer, Color> HighlightedRenderers = new Dictionary<Renderer, Color>();
 }
